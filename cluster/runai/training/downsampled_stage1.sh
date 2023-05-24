be87bd8bfd5584df7b4c1c6b8a20fcfe9d22521d
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-05-24 18:18:59 +0100

    Add downsampled

--- cluster/runai/training/downsampled_stage1.sh
+++ cluster/runai/training/downsampled_stage1.sh
@@ -1,17 +1,17 @@
 seed=42
-run_dir="aekl_v0"
+run_dir="downsampled_aekl_v0"
 training_ids="/project/outputs/ids/train.tsv"
 validation_ids="/project/outputs/ids/validation.tsv"
 config_file="/project/configs/stage1/aekl_v0.yaml"
-batch_size=1
-n_epochs=20
+batch_size=2
+n_epochs=75
 adv_start=5
-eval_freq=1
+eval_freq=3
 num_workers=8
-experiment="AEKL"
+experiment="DOWN-AEKL"
 
 runai submit \
-  --name brain-aekl-v0 \
+  --name down-brain-aekl-v0 \
   --image aicregistry:5000/wds20:ldm_brain \
   --backoff-limit 0 \
   --gpu 1 \
@@ -24,7 +24,7 @@ runai submit \
   --volume /nfs/home/wds20/projects/generative_brain/:/project/ \
   --volume /nfs/project/AMIGO/Biobank/derivatives/super-res/:/data/ \
   --command -- bash /project/src/bash/start_script.sh \
-    python3 /project/src/python/training/train_aekl.py \
+    python3 /project/src/python/training/train_downsampled_aekl.py \
       seed=${seed} \
       run_dir=${run_dir} \
       training_ids=${training_ids} \
