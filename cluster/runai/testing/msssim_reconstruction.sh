0857b4f9e56c98d58e2889ef176f1b128f374c8d
Walter Hugo Lopez Pinaya <ianonimato@hotmail.com>
2023-06-05 16:50:11 +0100

    Add testing

--- cluster/runai/testing/msssim_reconstruction.sh
+++ cluster/runai/testing/msssim_reconstruction.sh
@@ -3,7 +3,7 @@ test_ids="/project/outputs/ids/test.tsv"
 config_file="/project/configs/stage1/aekl_v0.yaml"
 stage1_path="/project/outputs/trained_models/autoencoder.pth"
 seed=42
-batch_size=16
+batch_size=2
 num_workers=8
 
 runai submit \
@@ -19,11 +19,12 @@ runai submit \
   --project wds20 \
   --volume /nfs/home/wds20/projects/generative_brain/:/project/ \
   --volume /nfs/project/AMIGO/Biobank/derivatives/super-res/:/data/ \
-  --command -- python3 /project/src/python/testing/compute_msssim_reconstruction.py \
-      --seed=${seed} \
-      --output_dir=${output_dir} \
-      --test_ids=${test_ids} \
-      --batch_size=${batch_size} \
-      --config_file=${config_file} \
-      --stage1_path=${stage1_path} \
-      --num_workers=${num_workers}
+  --command -- bash /project/src/bash/start_script.sh \
+      python3 /project/src/python/testing/compute_msssim_reconstruction.py \
+      seed=${seed} \
+      output_dir=${output_dir} \
+      test_ids=${test_ids} \
+      batch_size=${batch_size} \
+      config_file=${config_file} \
+      stage1_path=${stage1_path} \
+      num_workers=${num_workers}
